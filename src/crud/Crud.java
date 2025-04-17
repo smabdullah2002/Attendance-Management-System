06fb6e7d0a64e0919ba1f6fb35139ba7935727ef
smabdullah2002 <sheikhmuhammad2002@gmail.com>
2025-04-17 23:53:04 +0600

    new

--- src/crud/Crud.java
+++ src/crud/Crud.java
@@ -10,6 +10,8 @@ public class Crud {
 
   
     public static void main(String[] args) {
+        System.out.println("Hello World");
+        // Create a new instance of the AttendanceFrame class
         LoginForm loginObj = new LoginForm(); 
         loginObj.show();
          
